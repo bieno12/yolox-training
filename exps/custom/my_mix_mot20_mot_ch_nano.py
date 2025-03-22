fcba378c14d3c49728088a189930b96611bb8c15
bieno12 <zeyadshahin2015@gmail.com>
2025-03-22 16:55:55 +0200

    add nano exp

--- exps/custom/my_mix_mot20_mot_ch_nano.py
+++ exps/custom/my_mix_mot20_mot_ch_nano.py
@@ -12,15 +12,15 @@ class Exp(BaseExp):
         
         self.seed = 42
         
-        self.data_dir = "data/mix_mot20_ch"
+        self.data_dir = "data/mix_mot20_mot_ch"
         self.train_ann = "train.json"
         self.val_ann = "val_half.json"
         self.batch_size = 4
         self.test_ann = "test.json"
         
         self.num_classes = 1
-        self.depth = 1.33
-        self.width = 1.25
+        self.depth = 0.33
+        self.width = 0.25
         self.exp_name = os.path.split(os.path.realpath(__file__))[1].split(".")[0]
 
         self.input_size = (896, 1600)
@@ -51,6 +51,25 @@ class Exp(BaseExp):
 
         #override using env vars
         self.set_envvars()
+
+    def get_model(self, sublinear=False):
+
+        def init_yolo(M):
+            for m in M.modules():
+                if isinstance(m, nn.BatchNorm2d):
+                    m.eps = 1e-3
+                    m.momentum = 0.03
+        if "model" not in self.__dict__:
+            from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
+            in_channels = [256, 512, 1024]
+            # NANO model use depthwise = True, which is main difference.
+            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, depthwise=True)
+            head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, depthwise=True)
+            self.model = YOLOX(backbone, head)
+
+        self.model.apply(init_yolo)
+        self.model.head.initialize_biases(1e-2)
+        return self.model
     
     def set_envvars(self):
         for key, value in os.environ.items():
